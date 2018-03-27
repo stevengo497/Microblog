23999784fae4176c98e4154aa6c8cb3756392848
“stevengo497” <ngo.steve497@yahoo.com>
2018-03-27 17:40:23 -0600

    inputed text now showing on DOM

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const blogPage = document.getElementById("blogForm");
 
 	blogPage.addEventListener("submit", function(event) {
 		event.preventDefault();
+		// this allowed the button to be used
 		let newPost = document.getElementById("inputBox").value;
+		// this allowed the values inputed in the box to be recognized
 		console.log(newPost);
+// now lets add the inputed value into the HTML page so the user can see
+	let savedPost = document.getElementById('awesomePost') 
+	awesomePost.innerHTML += newPost
+
 	})
+
+// now lets add the inputed value into the HTML page so the user can see
+
 
