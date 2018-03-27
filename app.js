14d6c7e3376d38aecd11597be7a1914e3e871e71
“stevengo497” <ngo.steve497@yahoo.com>
2018-03-27 17:23:37 -0600

    input working and showing on console log when submitted

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 //Create form w/ text box
 // create submit box w/ submit event listener
 // add submitted value to html
+
+const blogPage = document.getElementById("blogForm");
+
+	blogPage.addEventListener("submit", function(event) {
+		event.preventDefault();
+		let newPost = document.getElementById("inputBox").value;
+		console.log(newPost);
+	})
+
