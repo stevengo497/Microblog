08a262eda5b3e341c3594fbe3f2735c9b617d7cc
“stevengo497” <ngo.steve497@yahoo.com>
2018-03-31 17:24:27 -0600

    Converted JS to jQuery

--- app.js
+++ app.js
@@ -2,22 +2,33 @@
 // create submit box w/ submit event listener
 // add submitted value to html
 
-const blogPage = document.getElementById("blogForm");
 
-	blogPage.addEventListener("submit", function(event) {
-		event.preventDefault();
+$(document).ready(function(event) {
+
+
+$('#blogForm').submit(function(event) {
+	event.preventDefault();
+
+$('#awesomePost').append("<li>" + ($('#inputBox').val()))
+
+})
+})
+
+// const blogPage = document.getElementById("blogForm");
+
+	// blogPage.addEventListener("submit", function(event) {
+	// 	event.preventDefault();
 		// this allowed the button to be used
-		let newPost = document.getElementById("inputBox").value;
-		// this allowed the values inputed in the box to be recognized
-		// console.log(newPost);
-// now lets add the inputed value into the HTML page so the user can see
-	let savedPost = document.getElementById('awesomePost') 
-	savedPost.innerHTML += newPost
+// 		let newPost = document.getElementById("inputBox").value;
+// 		// this allowed the values inputed in the box to be recognized
+// 		// console.log(newPost);
+// // now lets add the inputed value into the HTML page so the user can see
+// 	let savedPost = document.getElementById('awesomePost') 
+// 	savedPost.innerHTML += newPost
 
-	let bulletPoint = document.createElement("li");
-	savedPost.appendChild(bulletPoint);
+// 	let bulletPoint = document.createElement("li");
+// 	savedPost.appendChild(bulletPoint);
 	// console.log(bulletPoint);
 	// newPost
-	})
 
 //text are showing in line - need to show as vertical when new post added (appendChild?????)
